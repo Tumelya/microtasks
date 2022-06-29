ac0f75f3af3eb2e60b198030406bba259dbad5ae
Mariya Tumelya <mtumelia@gmail.com>
2022-06-29 15:43:13 +0300

    feat: add new component Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import {Header} from "./site/Header";
 import {Body} from "./site/Body";
+import {Footer} from "./site/Footer";
 
 function App() {
     return (
         <div>
             <Header/>
             <Body/>
+            <Footer/>
         </div>
     );
 }
