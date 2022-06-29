41134d2870eac1702c48ec98edc5c68a27a4b0ef
Mariya Tumelya <mtumelia@gmail.com>
2022-06-29 15:40:48 +0300

    feat: add new component Body

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Header} from "./site/Header";
+import {Body} from "./site/Body";
 
 function App() {
     return (
         <div>
             <Header/>
+            <Body/>
         </div>
     );
 }
---
+++ src/site/Body.tsx
@@ -0,0 +1,7 @@
+export const Body = () => {
+    return (
+        <>
+            <h3>Body</h3>
+        </>
+    )
+}
