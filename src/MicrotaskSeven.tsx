5503cf12f086a9d9ee5a66868c84a37543f98114
Mariya Tumelya <mtumelia@gmail.com>
2022-07-26 13:57:22 +0300

    feat: add the ability to add and remove tasks

--- src/MicrotaskSeven.tsx
+++ src/MicrotaskSeven.tsx
@@ -46,15 +46,13 @@ export const MicrotaskSeven = () => {
         ]
     });
 
-    function removeTask(id: string) {
-        /*let filteredTasks = tasks.filter(t => t.id != id);
-        setTasks(filteredTasks);*/
+    function removeTask(todolistID: string, id: string) {
+        setTasks({...tasks, [todolistID]: tasks[todolistID].filter(filtered => filtered.id != id)});
     }
 
-    function addTask(title: string) {
-        /*let task = {id: v1(), title: title, isDone: false};
-        let newTasks = [task, ...tasks];
-        setTasks(newTasks);*/
+    function addTask(todolistID: string, title: string) {
+        let newTask = {id: v1(), title: title, isDone: false};
+        setTasks({...tasks, [todolistID]: [...tasks[todolistID], newTask]});
     }
 
     function changeStatus(taskId: string, isDone: boolean) {
